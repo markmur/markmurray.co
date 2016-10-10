74a0827ada777bbda0bd374a98e307a2daf2e2eb
Mark Murray <mmmarkmurray@gmail.com>
2016-10-09 20:17:14 -0700

    analyze package script + remove moment

--- src/components/Photos/Photos.jsx
+++ src/components/Photos/Photos.jsx
@@ -1,11 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { firebase, helpers } from 'redux-firebasev3';
-import moment from 'moment';
-
-import Project from 'Project/Project';
 const { dataToJS, pathToJS } = helpers;
-
 
 const propTypes = {};
 const defaultProps = {};
@@ -26,19 +22,7 @@ class Photos extends Component {
 
   render() {
     return (
-      <div class="Photos">
-        <img src="/public/images/baybridge.jpg" role="presentation" />
-        <img src="/public/images/berkeleycat.jpg" role="presentation" />
-        <img src="/public/images/berkeleyhills.jpg" role="presentation" />
-        <img src="/public/images/eoinbokeh.jpg" role="presentation" />
-        <img src="/public/images/makeup.jpg" role="presentation" />
-        <img src="/public/images/motoroway.jpg" role="presentation" />
-        <img src="/public/images/silhouette.jpg" role="presentation" />
-        <img src="/public/images/twinpeaks.jpg" role="presentation" />
-        <img src="/public/images/twinpeakscar.jpg" role="presentation" />
-        <img src="/public/images/vangough.jpg" role="presentation" />
-        <img src="/public/images/venicebeach.jpg" role="presentation" />
-      </div>
+      <div class="Photos"></div>
     );
   }
 }
